79cf916383fc6d5a2e84ebe952079742ef87339f
Tomek Wąsik <wasik.tomasz.wro@gmail.com>
2021-02-17 18:42:05 +0100

    Initialize project

---
+++ src/main/java/com/app/kt_bracket/KtBracketApplication.java
@@ -0,0 +1,13 @@
+package com.app.kt_bracket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtBracketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtBracketApplication.class, args);
+	}
+
+}
